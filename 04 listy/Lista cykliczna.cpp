d7e7334ab555c6ca088bdd70458969d6d17367c7
JakubKwasniewskii <32430@s.pm.szczecin.pl>
2026-05-11 20:20:41 +0200

    Add files via upload

---
+++ 04 listy/Lista cykliczna.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    Node* tail;
+    Node(int val) : data(val), next(tail), tail(next) {}
+};
+
+void pushFront(Node*& head, int val) {
+    Node* newNode = new Node(val);
+    Node* temp = newNode->next;
+    newNode->next = head;
+    newNode->tail = temp;
+    head = newNode;
+}
+
+void popFront(Node*& head) {
+    if (!head) return;
+    Node* temp = head;
+    head->tail = head->next;
+    head = head->next;
+    delete temp;
+}
+
+void printList(Node* head) {
+    if (!head) return;
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "Polish");
+
+    Node* head = nullptr;
+    pushFront(head, 10);
+    pushFront(head, 20);
+    pushFront(head, 30);
+    printList(head);
+    popFront(head);
+    printList(head);
+
+    return 0;
+}
+
+
---
+++ 04 listy/Lista dwukierunkowa.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+struct DNode 
+{
+    int data;
+    DNode* prev;
+    DNode* next;
+    DNode(int val) : data(val), prev(nullptr), next(nullptr){}
+};
+
+void pushFront(DNode*& head, int val) {
+    DNode* newNode = new DNode(val);
+    if (head) head->prev = newNode;
+    newNode->next = head;
+    head = newNode;
+}
+
+void popBack(DNode*& head) {
+    if (!head) return;
+    DNode* temp = head;
+    while (temp->next) temp = temp->next;
+    if (temp->prev) temp->prev->next = nullptr;
+    else head = nullptr;
+    delete temp;
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "Polish");
+
+    DNode* head = nullptr;
+    pushFront(head, 10);
+    pushFront(head, 20);
+    pushFront(head, 30);
+
+    int n;
+    cout << "Ile losowych elementów dodać do listy ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        pushFront(head, rand() % 100);
+    }
+
+    cout << "Zawartość listy: ";
+    DNode* temp = head;
+
+    while (temp != nullptr) {
+        cout << temp->data;
+        if (temp->next != nullptr) cout << " <-> ";
+        temp = temp->next;
+    }
+    cout << " -> NULL" << endl;
+
+ 
+
+    return 0;
+}
+
