01fad1d5386ba6dc4ce33be24b269675de16a02f
JakubKwasniewskii <32430@s.pm.szczecin.pl>
2026-05-24 19:32:59 +0200

    Add files via upload

---
+++ 05 stosy i kolejki/Stosy.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int MAX = 100;
+const string stackEmptyInfo = "Stack is empty. ";
+
+class Stack {
+    int arr[MAX];
+    int top = -1;
+public:
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    bool isFull() {
+        return top == MAX - 1;
+    }
+    void push(int x) {
+        if (isFull()) {
+            cout << "Stack is Full.\n";
+            return;
+        }
+        arr[++top] = x;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << stackEmptyInfo << "\n";
+            return;
+        }
+        top--;
+    }
+    int peek() {
+        if (!isEmpty())
+            return arr[top];
+        cout << stackEmptyInfo << "\n";
+        return -1;
+
+    }
+
+    int getSize() {
+        return top + 1;
+    }
+
+    void printStack() {
+        if (isEmpty()) {
+            cout << stackEmptyInfo << "\n";
+            return;
+        }
+
+        for (int i = top; i >= 0; i--) {
+            cout << arr[i] << "\n";
+        }
+    }
+
+    void resetStack() {
+        top = -1;
+    }
+};
+int main()
+{
+    setlocale(LC_CTYPE, "Polish");
+
+    Stack mojStos;
+    cout << "Czy stos jest pusty? (1 = Tak, 0 = Nie): " << mojStos.isEmpty() << "\n";
+
+    mojStos.push(10);
+    mojStos.push(20);
+    mojStos.push(30);
+
+    cout << "Element na samej górze stosu: " << mojStos.peek() << "\n";
+    cout << "Czy stos jest pełny?: " << mojStos.isFull() << "\n";
+    mojStos.pop();
+    cout << "Teraz na górze jest: " << mojStos.peek() << "\n";
+
+    int liczbaElementow;
+    cout << "Podaj liczbę elementów" << endl;
+    cin >> liczbaElementow;
+
+    for (int i = 0; i < liczbaElementow; i++) {
+        int losowaLiczba = rand() % 100;
+        mojStos.push(losowaLiczba);
+        cout << losowaLiczba << " " << endl;
+    }
+
+    cout << "Liczba elementów w stosie wynosi: " << mojStos.getSize() << endl;
+
+    if (liczbaElementow == mojStos.getSize()) {
+        cout << "Wyniki się zgadzają " << liczbaElementow << " liczba elementów w stosie: " << mojStos.getSize() << endl;
+    }
+    else {
+        cout << "Liczby się nie zgadzają" << endl;
+    }
+
+    mojStos.printStack();
+
+    mojStos.resetStack();
+    mojStos.printStack();
+    mojStos.push(50);
+    mojStos.push(60);
+    mojStos.push(70);
+    mojStos.printStack();
+    mojStos.pop();
+    mojStos.printStack();
+
+    return 0;
+}
---
+++ 05 stosy i kolejki/kolejka.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int MAX = 100;
+const string queueEmptyInfo = "Queue is empty. ";
+
+class Queue {
+    int arr[MAX];
+    int first = 0;
+    int last = 0;
+
+public:
+    bool isEmpty() {
+        return first == last;
+    }
+
+    bool isFull() {
+        return last == MAX;
+    }
+
+    void enqueue(int x) {
+        if (isFull()) {
+            cout << "Queue is full.\n";
+            return;
+        }
+        arr[last++] = x;
+    }
+
+    void dequeue() {
+        if (isEmpty()) {
+            cout << queueEmptyInfo << "\n";
+            return;
+        }
+        first++;
+    }
+
+    int peek() {
+        if (!isEmpty())
+            return arr[first];
+        cout << queueEmptyInfo << "\n";
+        return -1;
+    }
+
+    int getSize() {
+        return last - first;
+    }
+
+    void printQueue() {
+        if (isEmpty()) {
+            cout << queueEmptyInfo << "\n";
+            return;
+        }
+        for (int i = last - 1; i >= first; i--) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    void resetQueue() {
+        first = 0;
+        last = 0;
+    }
+
+};
+
+int main()
+{
+    setlocale(LC_CTYPE, "Polish");
+
+    Queue mojaKolejka;
+    cout << "Czy kolejka jest pusta? (1 = Tak, 0 = Nie): " << mojaKolejka.isEmpty() << "\n";
+
+    mojaKolejka.enqueue(10);
+    mojaKolejka.enqueue(20);
+    mojaKolejka.enqueue(30);
+
+    cout << "Pierwszy element w kolejce to: " << mojaKolejka.peek() << "\n";
+    cout << "Czy kolejka jest pełna?: " << mojaKolejka.isFull() << "\n";
+    mojaKolejka.dequeue();
+    cout << "Teraz pierwszy element w kolejce to: " << mojaKolejka.peek() << "\n";
+
+    int liczbaElementow;
+    cout << "Podaj liczbę elementów do dodania do kolejki: ";
+    cin >> liczbaElementow;
+
+    for (int i = 0; i < liczbaElementow; i++) {
+        int losowaLiczba = rand() % 100; 
+        mojaKolejka.enqueue(losowaLiczba);
+        cout << losowaLiczba << " " << endl;
+    }
+
+    cout << "Liczba elementów w kolejce wynosi: " << mojaKolejka.getSize() << "\n";
+
+    if (liczbaElementow == mojaKolejka.getSize()) {
+        cout << "Wyniki się zgadzają " << liczbaElementow << "liczba elementów w kolejce: " << mojaKolejka.getSize() << "\n";
+    }
+    else {
+        cout << "Liczby się nie zgadzają.\n";
+    }
+
+    mojaKolejka.printQueue();
+
+    mojaKolejka.resetQueue();
+    mojaKolejka.printQueue();
+    mojaKolejka.enqueue(55);
+    mojaKolejka.enqueue(65);
+    mojaKolejka.enqueue(75);
+    mojaKolejka.printQueue();
+    mojaKolejka.dequeue();
+    mojaKolejka.printQueue();
+
+    return 0;
+}
